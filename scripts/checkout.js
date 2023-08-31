5eac82a60788235467c1a4510655ceafaf611571
Harsh <harshitmor72@gmail.com>
2023-08-31 22:13:24 +0530

    delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart , removeFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -14,7 +14,6 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-
 
   cartSummaryHTMl += `
     <div class="cart-item-container">
@@ -35,12 +34,14 @@ cart.forEach((cartItem) => {
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                    Quantity: <span class="quantity-label">${
+                      cartItem.quantity
+                    }</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -95,4 +96,11 @@ cart.forEach((cartItem) => {
     `;
 });
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTMl;
+document.querySelector(".js-order-summary").innerHTML = cartSummaryHTMl;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+  });
+});
